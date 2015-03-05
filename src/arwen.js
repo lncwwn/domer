a9c6f88c444d7da9c86b02809b81fb1963aeff8b
victor.li <lncwwn@gmail.com>
2015-03-05 18:13:08 +0800

    refined code

---
+++ src/arwen.js
@@ -0,0 +1,21 @@
+/**
+ * Arwen is a DOM selector.
+ * @author victor li
+ * @date 03/05/2015
+ * @version 0.0.1
+ * Released under terms of MIT lincense
+ */
+var Arwen, $$;
+
+;(function(window, undefined) {
+    Arwen = function(symbol) {
+        //
+    };
+
+    var _select = function(symbol) {
+        //
+    };
+
+    //var 
+
+})(window);
